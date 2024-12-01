e7506f64f20a2e06aa006d13466b7da748c0e743
aarati05 <aarati.gurung@estudiant.fjaverianas.com>
2024-12-01 15:42:21 +0100

    slay

--- m8ac7.py
+++ m8ac7.py
@@ -1,21 +1,21 @@
 from flask import __version__,Flask, render_template;
 import sys;
-#import mysql.connector
+import mysql.connector
 
 app = Flask(__name__)
 
-# def registerConnection():
-#    db = mysql.connector.connect(user='usuari1', password='',
-#                               host='localhost',
-#                               database='m8ac7')
-#    cursor = db.cursor()
-#    cursor.execute("insert into prova() values();")
-#    db.commit()
-#    db.close()
+def registerConnection():
+    db = mysql.connector.connect(user='usuari1', password='',
+                               host='localhost',
+                               database='m8ac7')
+    cursor = db.cursor()
+    cursor.execute("insert into prova() values();")
+    db.commit()
+    db.close()
 
 @app.route('/') 
 def hello_world():
-   # registerConnection()
+   registerConnection()
    pyversion = sys.version_info[0]
    flaskversion = __version__
    return render_template("index.html",
